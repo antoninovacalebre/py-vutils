da82960146d022b601678ff99829ebf6a9981741
Antonino Vacalebre <vacalebre.antonino@gmail.com>
2022-10-20 10:00:30 +0200

    aggiunta versione basica per progress_bar

--- build/lib/vutils/progress_bar.py
+++ build/lib/vutils/progress_bar.py
@@ -26,24 +26,28 @@ def print_progress_bar(iteration, total, prefix='', suffix='', usePercentage=Fal
     if iteration == total:
         print()
 
-def pb_init(text:str) -> int:
-    print(text, end='')
-    return 0
+def print_progress_bar(iteration, total, prefix='', suffix='', decimals=1, length=80, fill='█', printEnd="\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                     (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {iteration:>{len(f"{total}")}}/{total} {suffix}', end=printEnd)
 
-def pb_update(iter:int, old_nbars:int, total:int, length:int = 80) -> int:
-    nbars = int(length * iter // total)
-    ten = length // 10
-    
-    for i in range(old_nbars, nbars):
-        imod10 = i % ten
-        if (imod10 == 0 and i != 0):
-            print(i // ten * 10, end='')
-        print('-', end='')
-
-    if iter == total:
-        print(100)
-
-    return nbars
+    # Print New Line on Complete
+    if iteration == total:
+        print()
 
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, printEnd="\r"):
     """
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='vutils',
-                 version='1.0',
+                 version='1.2',
                  description='Useful stuff',
                  url='#',
                  author='av',
